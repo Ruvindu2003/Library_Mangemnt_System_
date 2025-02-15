1704ed6a6f3bd16e2b7b24ea3def1a22d970cba8
Ruvindu <ruvindusharada22@gmail.com>
2025-02-15 12:16:34 +0530

    pomt file create

--- src/main/java/Forms/Report_Form.java
+++ src/main/java/Forms/Report_Form.java
@@ -2,12 +2,22 @@ package Forms;
 
 import Controller.ReportController;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.util.StringConverter;
 
-public class Report_Form {
+import java.net.URL;
+import java.text.BreakIterator;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class Report_Form implements Initializable {
     public CheckBox chbox_books;
     public CheckBox chombox_member;
     public CheckBox chobox_returnbooks;
@@ -22,16 +32,69 @@ public class Report_Form {
     ReportController reportController=new ReportController();
 
     public void btn_gemerateReporton_Action(ActionEvent actionEvent) {
-        if(Integer.parseInt(txt_book_count.getText())<0){
-            reportController.Generate_Reportby_Count("src/main/resources/reports/Addbook3.jrxml","booksreport.pdf","SELECT * FROM books LIMIT 5");
-
-        }else {
-
+        if (!(isNumber(txt_book_count)) || Integer.parseInt(txt_book_count.getText()) < 0 || txt_book_count.getText().isEmpty()){
+            new Alert(Alert.AlertType.INFORMATION,"Invalid input, please check the fields.").show();
+            return;
         }
 
+        if(chbox_books.isSelected()){
+            reportController.Generate_Reportby_Count("src/main/resources/Reports/Addbook3.jrxml","booksreport.pdf","SELECT * FROM books LIMIT " + Integer.valueOf(txt_book_count.getText()));
+        }
 
-        reportController.Generate_Reportby_member("src/main/resources/Reports/Addmember4.jrxml/","member.pdf","SELECT * FROM members LIMIT 5");
+        if(chobox_borrowedbooks.isSelected()){
+            reportController.Generate_Reportby_Count("src/main/resources/Reports/broowedbooksReport.jrxml","borrowedbooksreport.pdf","SELECT * FROM borrowedbooks WHERE borrow_date >= '"+dt_browwedboks.getValue().toString()+"' AND borrow_date < '"+txt_bb_nowdate.getText()+"';");
+        }
+
+        if(chobox_returnbooks.isSelected()){
+            reportController.Generate_Reportby_Count("src/main/resources/Reports/Returnbooks.jrxml","returnedbooksreport.pdf","SELECT * FROM returnsbooks WHERE return_date>= '"+dt_returned_books.getValue().toString()+"' AND return_date < '"+txt_rb_nowdate.getText()+"';");
+        }
+
+        if(chombox_member.isSelected()){
+            reportController.Generate_Reportby_Count("src/main/resources/Reports/AddmemberReport.jrxml","memberreport.pdf","SELECT * FROM members LIMIT " + Integer.valueOf(txt_member_count.getText()));
+        }
     }
+
+    public boolean isNumber(TextField textField) {
+        try {
+            Double.parseDouble(textField.getText());
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private void LoadDate() {
+        Date date = new Date();
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+        BreakIterator lblDate;
+        txt_bb_nowdate.setText(f.format(date));
+        txt_rb_nowdate.setText(f.format(date));
+    }
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private void configureDatePicker(DatePicker datePicker) {
+        datePicker.setConverter(new StringConverter<LocalDate>() {
+            @Override
+            public String toString(LocalDate date) {
+                return (date != null) ? formatter.format(date) : "";
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                return (string != null && !string.isEmpty()) ? LocalDate.parse(string, formatter) : null;
+            }
+        });
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        LoadDate();
+        configureDatePicker(dt_returned_books);
+        configureDatePicker(dt_browwedboks);
+
+    }
+
 
 
 }
